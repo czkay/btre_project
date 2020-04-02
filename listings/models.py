71934807d873703732346674ae80bf3cae159d11
czkay <chong.zi.kang@gmail.com>
2020-04-02 17:35:47 +0800

    Improve admin site styling

--- listings/models.py
+++ listings/models.py
@@ -18,8 +18,6 @@ class Listing(models.Model):
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-    title = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
