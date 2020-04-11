86ea825d3e6271530f833cec7700adab23b599cc
czkay <chong.zi.kang@gmail.com>
2020-04-12 00:07:37 +0800

    Add dynamic Search page

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import bedroom_choices, price_choices, state_choices
 from listings.models import Listing
 
 # Create your views here.
@@ -24,4 +25,9 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices
+    }
+    return render(request, 'listings/search.html', context)
