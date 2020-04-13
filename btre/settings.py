4c90b1de4b2b7f8580d07dd503c2c0374c259f99
czkay <chong.zi.kang@gmail.com>
2020-04-13 22:42:32 +0800

    Remove email config

--- btre/settings.py
+++ btre/settings.py
@@ -145,14 +145,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Email config
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'randomstuffies@gmail.com'
-EMAIL_HOST_PASSWORD = 'Iwakmp18'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
