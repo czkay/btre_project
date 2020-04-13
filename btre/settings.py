8cabaa261c3fa4513cd63c86cc4bf3371cfa52ba
czkay <chong.zi.kang@gmail.com>
2020-04-13 22:37:00 +0800

    Add local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -152,3 +152,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'randomstuffies@gmail.com'
 EMAIL_HOST_PASSWORD = 'Iwakmp18'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
